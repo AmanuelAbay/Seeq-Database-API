b5a11b3a01360fe0ac2dd32fe11c4044e7a749c2
AmanuelAbay <amanab.449@gmail.com>
2022-04-10 01:20:54 +0300

    ticket preview for users

--- app/customerApp.js
+++ app/customerApp.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use('/seeq/api/user', customerRouter);
 app.use('/seeq/api/events', eventsRouter);
-app.use('/seeq/api/events/ticket', eventTicketRouter);
+app.use('/seeq/api/events/tickets', eventTicketRouter);
 app.all('*', (req, res, next) => {
     res.status(404).json({
         status: 'FAIL',
